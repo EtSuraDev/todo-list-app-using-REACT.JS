f9f081b77d389eace9db36f440e5e16262eb7a69
surafel meles <melessurafel413@gmail.com>
2024-09-30 16:01:55 +0300

    v.1

--- src/button.jsx
+++ src/button.jsx
@@ -1,5 +1,70 @@
 import { useState } from "react";
 function Button(){
+    let [toDoItem,setToDoItem] = useState([])
+    function handleSaveBtn(){
+        let input = document.querySelector(".input>input").value
+        document.querySelector(".input>input").value = ""
+        setToDoItem([...toDoItem,input])
+            console.log(toDoItem)
+    }
+    function mapToDoItem(){
+        return(toDoItem.map((item,index) => <tr className="data">
+                                    <td className="no-data">
+                                        {index + 1}
+                                    </td>
+                                    <td className="Todo-item-data">
+                                        {item}
+                                    </td>
+                                    <td className="Status-data">
+                                        <p>in progres</p>
+                                    </td>
+                                    <td className="Actions-data">
+                                        <button className="delete" onClick={() => handleDeleteBtn(index)}>Delete</button>
+                                        <br />
+                                        <button className="done" id={index} onClick={ handleDoneBtn}>Done!</button>
+                                    </td>
+                                </tr>
+                            )
+                        )
+    }
+    function handelToDoItemHeading(){
+        if(toDoItem.length > 0){
+            return(
+                <table>
+                    <tr className="heading">
+                        <th className="no-th">
+                            No.
+                        </th>
+                        <th className="Todo-item-th">
+                            Todo item
+                        </th>
+                        <th className="Status-th">
+                            
+                        </th>
+                        <th colSpan={2} className="Actions-th">
+                            Actions
+                        </th>
+                    </tr>
+                    { mapToDoItem() }
+                </table>
+            )
+        }else{
+            return null
+        }
+    }
+    function handleDoneBtn(e){
+        let num = e.target.id;
+        console.log(num)
+        let x = document.querySelectorAll(".Status-data>p")
+        x[num].innerText = "Done!"
+        x[num].style.color = "rgb(15, 190, 15)"
+        e.target.style.display = "none"
+    }
+    function handleDeleteBtn(index){
+        let x = toDoItem.filter((_,i) => i !== index)
+        setToDoItem(x)
+    }
+
     
     return(
         <>
@@ -11,74 +76,10 @@ function Button(){
                 </div>
                 <div className="input">
                     <input type="text" placeholder="Enter a task here"/>
-                    <button>Save</button>
+                    <button onClick={ handleSaveBtn }>Save</button>
                 </div>
                 <div className="todo-list">
-                    <table>
-                        <tr>
-                            <th className="no-th">
-                                No.
-                            </th>
-                            <th className="Todo-item-th">
-                                Todo item
-                            </th>
-                            <th className="Status-th">
-                                Status
-                            </th>
-                            <th colSpan={2} className="Actions-th">
-                                Actions
-                            </th>
-                        </tr>
-                        <tr className="data">
-                            <td className="no-data">
-                                1
-                            </td>
-                            <td className="Todo-item-data">
-                                my name is surafel meles 2lqhdioh qwejdhioqs qio
-                            </td>
-                            <td className="Status-data">
-                                <p>in progres</p>
-                            </td>
-                            <td className="Actions-data">
-                                <button className="delete">Delete</button>
-                                <br />
-                                <button className="done">Done!</button>
-                            </td>
-                        </tr>
-                        <tr className="data">
-                            <td className="no-data">
-                                2
-                            </td>
-                            <td className="Todo-item-data">
-                                my name is surafel meles 
-                            </td>
-                            <td className="Status-data">
-                                <p>in progres</p>
-                            </td>
-                            <td className="Actions-data">
-                                <button className="delete">Delete</button>
-                                <br />
-                                <button className="done">Done!</button>
-                            </td>
-                        </tr>
-                        <tr className="data">
-                            <td className="no-data">
-                                3
-                            </td>
-                            <td className="Todo-item-th">
-                                my name is surafel meles 
-                            </td>
-                            <td className="Status-data">
-                                <p>in progres</p>
-                            </td>
-                            <td className="Actions-data">
-                                <button className="delete">Delete</button>
-                                <br />
-                                <button className="done">Done!</button>
-                            </td>
-                        </tr>
-                    </table>
-                    
+                    {handelToDoItemHeading()}
                 </div>
             </div>
         </>
